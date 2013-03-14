f53cb7fcd3ee84c11af6f9feb28fa1fb20332cf2
Nir Le <kirlev@gmail.com>
2013-03-14 12:42:16 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KirRailsTest::Application.config.secret_token = '5662be331758189bfb4a45b019e9638879a80dc50ed42615e049052f7a2edea8b827f8b37d816cc0a2a1f683026f8923d5eb88259496bd4d5019973f5dc0a225'
